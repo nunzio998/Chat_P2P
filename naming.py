689f873fb46f6f5593ff0b5b6f4543754a5dd126
Gabriele Giordano <giordano.gabriele@outlook.it>
2023-06-16 21:38:09 +0200

    aggiunto file di gestione dei nickname

---
+++ naming.py
@@ -0,0 +1,24 @@
+import re
+
+
+def check_name(name: str) -> bool:
+    """
+    Funzione che controlla che la stringa in ingresso rispetti il formato XXXXXXdd, in cui X è qualsiasi carattere,
+    maiuscolo o minuscolo, dell'alfabeto inglese da A a Z, e d rappresenta una qualsiasi cifra da 0 a 9.
+
+    :param name:
+    :return: restituisce true se la stringa rappresenta un nome valido
+    """
+    if not re.match(r'^[A-Za-z]{6}\d{2}$', name):
+        raise ValueError('Il nickname inserito non rispetta i parametri..')
+    return True
+
+
+def format_name(name:str) -> str:
+    """
+    Formatta la stringa in ingresso in XXXXXXdd con X carattere maiuscolo
+
+    :param name:
+    :return:
+    """
+    return name.upper()
