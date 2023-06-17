b7ce3c4494539336de75c11bc3fba94285d5a334
Matteo Gardusi <matteo.gardusi@gmail.com>
2023-06-17 18:07:40 +0200

    aggiunta message_handler_utils.py

---
+++ message_handler_utils.py
@@ -0,0 +1,31 @@
+def ack_message_handler(my_node_id, id_destinatario, id_mittente, message, socket_send, ip_next, port_next):
+    """
+    Funzione che ha il compito gestire i messaggi di tipo ACK.
+    :param my_node_id:
+    :param id_destinatario:
+    :param id_mittente:
+    :param message:
+    :param socket_send:
+    :param ip_next:
+    :param port_next:
+    :return:
+    """
+
+    pass
+
+
+def standard_message_handler(my_node_id, id_destinatario, id_mittente, message, socket_send, ip_next, port_next):
+    """
+    Funzione che ha il compito gestire i messaggi di tipo STANDARD.
+    :param my_node_id:
+    :param id_destinatario:
+    :param id_mittente:
+    :param message:
+    :param socket_send:
+    :param ip_next:
+    :param port_next:
+    :return:
+    """
+
+    pass
+
